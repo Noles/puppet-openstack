ae274249b0c79aaca4e0c8d9a068eca0708f1a17
Alexander Korinek <noles@a3k.net>
2016-12-23 15:57:08 +0100

    Create allinone.pp

---
+++ manifest/allinone.pp
@@ -0,0 +1,10 @@
+include ::openstack
+include ::openstack::rabbitmq
+include ::openstack::mysql
+include ::openstack::keystone
+include ::openstack::glance
+include ::openstack::neutron
+include ::openstack::nova
+include ::openstack::cinder
+include ::openstack::horizon
+include ::openstack::provision

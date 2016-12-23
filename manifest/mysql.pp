cba0551635e887c2a81138c8ba390150567de5ed
Alexander Korinek <noles@a3k.net>
2016-12-23 16:09:05 +0100

    Create mysql.pp

---
+++ manifest/mysql.pp
@@ -0,0 +1,14 @@
+class openstack::mysql {
+
+  class { '::mysql::server': }
+
+  # FIXME (amoralej) Required until a new release of mariadb-libs is released by RDO
+  if $::osfamily == 'RedHat' {
+    package { 'mariadb-libs':
+      ensure => 'latest'
+    }
+    Package['mariadb-libs'] -> Class['mysql::server']
+    Package['mariadb-libs'] -> Class['mysql::client']
+  }
+
+}

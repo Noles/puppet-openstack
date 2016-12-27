230c422ec01163b25fb92f6cb83ef1d099c4259a
Alexander Korinek <noles@a3k.net>
2016-12-27 08:38:18 +0100

    Update presetup.sh

--- presetup.sh
+++ presetup.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+end_of_file=0
+while [[ $end_of_file == 0 ]]
+do
+  read -r line
+  # the last exit status is the 
+  # flag of the end of file
+  end_of_file=$?
+  echo $line
+  git clone http://git.openstack.org/openstack/$line /etc/puppet/modules/
+done < "openstack_modules.txt"
+
+end_of_file=0
+while [[ $end_of_file == 0 ]]
+do
+  read -r line
+  # the last exit status is the 
+  # flag of the end of file
+  end_of_file=$?
+  echo $line
+  puppet module install $line
+done < "external_modules.txt"

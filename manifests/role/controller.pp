3d2330ffb13676315bfc85da6c9a2a8a7c8e7591
Alexander Korinek <noles@a3k.net>
2016-12-28 10:31:18 +0100

    Create controller.pp

---
+++ manifests/role/controller.pp
@@ -0,0 +1,14 @@
+class openstack::role::allinone inherits ::openstack::role {
+  class { '::openstack::rabbitmq': }
+  class { '::openstack::mysql': }
+  class { '::openstack::keystone': }
+ 
+ #TODO
+  #class { '::openstack::glance': }
+  #class { '::openstack::neutron': }
+  #class { '::openstack::nova': }
+  #class { '::openstack::cinder': }
+  
+  class { '::openstack::horizon': }
+  #class { '::openstack::provision': }
+}

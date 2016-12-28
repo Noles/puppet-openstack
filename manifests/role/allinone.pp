3ca826907b5d82464386aaf603719c857fe38881
Alexander Korinek <noles@a3k.net>
2016-12-28 10:08:49 +0100

    Update allinone.pp

--- manifests/role/allinone.pp
+++ manifests/role/allinone.pp
@@ -1,3 +1,10 @@
 class openstack::role::allinone inherits ::openstack::role {
-
+  class { '::openstack::rabbitmq': }
+  class { '::openstack::mysql': }
+  class { '::openstack::keystone': }
+  class { '::openstack::glance': }
+  class { '::openstack::neutron': }
+  class { '::openstack::nova': }
+  class { '::openstack::cinder': }
+  class { '::openstack::horizon': }
 }

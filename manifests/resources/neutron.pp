0b2f4616c106f73adf4360cadacf258994693938
Alexander Korinek <noles@a3k.net>
2016-12-28 11:24:02 +0100

    Update neutron.pp

--- manifests/resources/neutron.pp
+++ manifests/resources/neutron.pp
@@ -1,5 +1,11 @@
 class openstack::resource::neutron {
 
+  class { '::neutron::keystone::auth':
+    public_url   => "${::openstack::config::base_url}:9696",
+    internal_url => "${::openstack::config::base_url}:9696",
+    admin_url    => "${::openstack::config::base_url}:9696",
+    password     => 'a_big_secret',
+  }
   class { '::neutron':
     default_transport_url => os_transport_url({
       'transport' => 'rabbit',

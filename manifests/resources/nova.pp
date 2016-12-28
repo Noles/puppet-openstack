23931a21ac01aecf88c614394ada1a2500d68b79
Alexander Korinek <noles@a3k.net>
2016-12-28 11:03:35 +0100

    Update nova.pp

--- manifests/resources/nova.pp
+++ manifests/resources/nova.pp
@@ -1,4 +1,22 @@
 class openstack::resource::nova {
-  include ::openstack::config
-  include ::openstack::params  
+
+  $transport_url = os_transport_url({
+    'transport' => 'rabbit',
+    'host'      => $::openstack::config::host,
+    'port'      => $::openstack::config::rabbit_port,
+    'username'  => 'nova',
+    'password'  => 'an_even_bigger_secret',
+  })
+
+  class { '::nova':
+    default_transport_url   => $transport_url,
+    database_connection     => 'mysql+pymysql://nova:nova@127.0.0.1/nova?charset=utf8',
+    api_database_connection => 'mysql+pymysql://nova_api:nova@127.0.0.1/nova_api?charset=utf8',
+    rabbit_use_ssl          => $::openstack::config::ssl,
+    use_ipv6                => $::openstack::config::ipv6,
+    glance_api_servers      => "${::openstack::config::base_url}:9292",
+    debug                   => true,
+    notification_driver     => 'messagingv2',
+    notify_on_state_change  => 'vm_and_task_state',
+  }
 }
